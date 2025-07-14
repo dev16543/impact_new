9eed14e307d550a7067a62022ef18b0281c5b42d
Ansh Agarwal <anshagarwal.rishikesh@gmail.com>
2025-07-15 00:18:48 +0530

    chn

--- my-vite-app/src/pages/Home.jsx
+++ my-vite-app/src/pages/Home.jsx
@@ -1,31 +1,12 @@
 import Beams from '../Component/Beams';
 import React from 'react';
 
+
 const HomePage = () => {
 return (
-<div style={{ position: 'relative', zIndex: 0 }}>
-  {/* Background Layer */}
-  <div
-    style={{
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      width: '100vw',
-      height: '100vh',
-      zIndex: -1,
-    }}
-  >
-    <Beams
-      beamWidth={8}
-      beamHeight={30}
-      beamNumber={22}
-      lightColor="#ffffff"
-      speed={7}
-      noiseIntensity={1.75}
-      scale={0.2}
-      rotation={40}
-    />
-  </div>
+<div>
+
+  <a className='bgcolor:blue'>Hello World</a>
 </div>
 
 )
